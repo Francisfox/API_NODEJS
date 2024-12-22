f4ec29492f777312e58c9b2630176259b43dc301
Francisfox <francismarfox@hotmail.com>
2024-12-22 14:16:01 +0000

    correção do server

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+// Middleware para servir arquivos estáticos da pasta "public"
+app.use(express.static('public'));
+
 // Rota para a página principal
 app.get('/', (req, res) => {
     res.send('<h1>Bem-vindo ao Node.js!</h1><p>Esta é uma página simples.</p>');
@@ -11,4 +14,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
-app.use(express.static('public'));
