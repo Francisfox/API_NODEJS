af183d6c2d04d12cb56696d2f11f3beb7540db4e
Francisfox <francismarfox@hotmail.com>
2024-12-22 13:46:47 +0000

    createServer

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # API_NODEJS
+
+https://gitpod.io/#https://github.com/Francisfox/API_NODEJS.git
+
+npm init -y
+npm install express
+touch server.js
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Rota para a página principal
+app.get('/', (req, res) => {
+    res.send('<h1>Bem-vindo ao Node.js!</h1><p>Esta é uma página simples.</p>');
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
